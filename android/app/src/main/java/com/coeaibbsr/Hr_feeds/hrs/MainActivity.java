2fbfa17cd4589c7b9f46ec0460a47414067fcaea
Manas Mohanty <coeaimrm@cet.edu.in>
2020-07-08 11:05:11 +0530

    first commit

---
+++ android/app/src/main/java/com/coeaibbsr/Hr_feeds/hrs/MainActivity.java
@@ -0,0 +1,6 @@
+package com.coeaibbsr.Hr_feeds.hrs;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/instant_feed.dart
@@ -0,0 +1,376 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:speech_recognition/speech_recognition.dart';
+
+class InstantFeedPage extends StatefulWidget {
+  @override
+  _InstantFeedPageState createState() => _InstantFeedPageState();
+}
+
+class _InstantFeedPageState extends State<InstantFeedPage> {
+
+  //read Pcount ,Ncount data from dashboard page assign it to pCount,nCount variable.
+
+
+  var selectedEmployee, selectedType,month,pCount=0,nCount=0;
+
+  final GlobalKey<FormState> _formKeyValue = new GlobalKey<FormState>();
+  List<String> _feedbackType = <String>[
+    'Positive ',
+    'Negative ',
+  ];
+
+  List<String> _monthType = <String>[
+    'Jan ',
+    'Feb ',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+
+  ];
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+
+  String resultText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechRecognizer();
+  }
+
+  void createRecord(selectedMonthType,selectedEmployee,selectedFeedbackType,resultText){
+    if (selectedFeedbackType == "Positive")
+    {
+      pCount+=1;
+    }
+    else
+    {
+      nCount+=1;
+    }
+    Firestore.instance.collection('$selectedMonthType').document('$selectedEmployee').collection('feeds').add({
+      'FeedBackType':'$selectedFeedbackType',
+      'FeedBack':'$resultText'
+    });
+
+    Firestore.instance.collection('$selectedMonthType').document('$selectedEmployee').setData(
+      {
+        'NCount':nCount,
+        'PCount':pCount
+      }
+
+    );
+
+
+
+
+
+  }
+
+  void initSpeechRecognizer() {
+    _speechRecognition = SpeechRecognition();
+
+    _speechRecognition.setAvailabilityHandler(
+          (bool result) => setState(() => _isAvailable = result),
+    );
+
+    _speechRecognition.setRecognitionStartedHandler(
+          () => setState(() => _isListening = true),
+    );
+
+    _speechRecognition.setRecognitionResultHandler(
+          (String speech) => setState(() => resultText = speech),
+    );
+
+    _speechRecognition.setRecognitionCompleteHandler(
+          () => setState(() => _isListening = false),
+    );
+
+    _speechRecognition.activate().then(
+          (result) => setState(() => _isAvailable = result),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: Icon(
+              FontAwesomeIcons.bars,
+              color: Colors.white,
+            ),
+            onPressed: () {}),
+        title: Container(
+          alignment: Alignment.center,
+          child: Text("HR App",
+              style: TextStyle(
+                color: Colors.white,
+              )),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              FontAwesomeIcons.bars,
+              size: 20.0,
+              color: Colors.white,
+            ),
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKeyValue,
+        autovalidate: true,
+        child: new ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          children: <Widget>[
+            new Text(
+              "Months",
+              style: new TextStyle(fontSize:40.0,
+                  color: const Color(0xFF312c2c),
+                  fontWeight: FontWeight.w300,
+                  fontFamily: "Roboto"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.bars,
+                  size: 25.0,
+                  color: Color(0xff11b719),
+                ),
+                SizedBox(width: 50.0),
+                DropdownButton(
+                  items: _monthType
+                      .map((value) => DropdownMenuItem(
+                    child: Text(
+                      value,
+                      style: TextStyle(color: Color(0xff11b719)),
+                    ),
+                    value: value,
+                  ))
+                      .toList(),
+                  onChanged: (selectedMonthType) {
+                    print('$selectedMonthType');
+                    setState(() {
+                      month = selectedMonthType;
+                    });
+                  },
+                  value: month,
+                  isExpanded: false,
+                  hint: Text(
+                    'Choose Month',
+                    style: TextStyle(color: Color(0xff11b719)),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            new Text(
+              "employee",
+              style: new TextStyle(fontSize:40.0,
+                  color: const Color(0xFF312c2c),
+                  fontWeight: FontWeight.w300,
+                  fontFamily: "Roboto"),
+            ),
+
+            StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance.collection("$month").snapshots(),
+                // ignore: missing_return
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData)
+                    const Text("Loading.....");
+
+                  else {
+                    List<DropdownMenuItem> employeeItems = [];
+                    for (int i = 0; i < snapshot.data.documents.length; i++) {
+                      DocumentSnapshot snap = snapshot.data.documents[i];
+                      employeeItems.add(
+                        DropdownMenuItem(
+                          child: Text(
+                            snap.documentID,
+                            style: TextStyle(color: Color(0xff11b719)),
+                          ),
+                          value: "${snap.documentID}",
+                        ),
+                      );
+                    }
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(FontAwesomeIcons.bars,
+                            size: 25.0, color: Color(0xff11b719)),
+                        SizedBox(width: 50.0),
+                        DropdownButton(
+                          items: employeeItems,
+                          onChanged: (employeeValue) {
+                            final snackBar = SnackBar(
+                              content: Text(
+                                'Selected Employee is $employeeValue',
+                                style: TextStyle(color: Color(0xff11b719)),
+                              ),
+                            );
+                            Scaffold.of(context).showSnackBar(snackBar);
+                            setState(() {
+                              selectedEmployee = employeeValue;
+                            });
+                          },
+                          value: selectedEmployee,
+                          isExpanded: false,
+                          hint: new Text(
+                            "Choose employee",
+                            style: TextStyle(color: Color(0xff11b719)),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                }),
+            SizedBox(
+              height: 50.0,
+            ),
+            new Text(
+              "Feedback type",
+              style: new TextStyle(fontSize:40.0,
+                  color: const Color(0xFF312c2c),
+                  fontWeight: FontWeight.w300,
+                  fontFamily: "Roboto"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.bars,
+                  size: 25.0,
+                  color: Color(0xff11b719),
+                ),
+                SizedBox(width: 50.0),
+                DropdownButton(
+                  items: _feedbackType
+                      .map((value) => DropdownMenuItem(
+                    child: Text(
+                      value,
+                      style: TextStyle(color: Color(0xff11b719)),
+                    ),
+                    value: value,
+                  ))
+                      .toList(),
+                  onChanged: (selectedFeedbackType) {
+                    print('$selectedFeedbackType');
+                    setState(() {
+                      selectedType = selectedFeedbackType;
+                    });
+                  },
+                  value: selectedType,
+                  isExpanded: false,
+                  hint: Text(
+                    'Choose Feedback Type',
+                    style: TextStyle(color: Color(0xff11b719)),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 40.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  child: Icon(Icons.cancel),
+                  mini: true,
+                  backgroundColor: Colors.deepOrange,
+                  onPressed: () {
+                    if (_isListening)
+                      _speechRecognition.cancel().then(
+                            (result) => setState(() {
+                          _isListening = result;
+                          resultText = "";
+                        }),
+                      );
+                  },
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.mic),
+                  onPressed: () {
+                    if (_isAvailable && !_isListening)
+                      _speechRecognition
+                          .listen(locale: "en_US")
+                          .then((result) => print('$result'));
+                  },
+                  backgroundColor: Colors.pink,
+                ),
+                FloatingActionButton(
+                  child: Icon(Icons.stop),
+                  mini: true,
+                  backgroundColor: Colors.deepPurple,
+                  onPressed: () {
+                    if (_isListening)
+                      _speechRecognition.stop().then(
+                            (result) => setState(() => _isListening = result),
+                      );
+                  },
+                ),
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(
+                color: Colors.cyanAccent[100],
+                borderRadius: BorderRadius.circular(6.0),
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 12.0,
+              ),
+              child: Text(
+                resultText,
+                style: TextStyle(fontSize: 24.0),
+              ),
+              //need to add font weight and style fontweight
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            RaisedButton(
+                color: Color(0xff11b719),
+                textColor: Colors.white,
+                child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text("Submit", style: TextStyle(fontSize: 24.0)),
+                      ],
+
+                    )),
+
+                    onPressed: () {
+
+                      createRecord(month,selectedEmployee,selectedType,resultText);
+                     },
+                     //Using Transactions
+                     // Firestore.instance.runTransaction((Transaction crudTransaction) async {
+                     //   CollectionReference reference =
+                     //       await Firestore.instance.collection('testcrud');
+
+                     //   reference.add(carData);
+                     // });
+
+
+                 shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0))),
+          ],
+        ),
+      ),
+    );
+  }
+}
