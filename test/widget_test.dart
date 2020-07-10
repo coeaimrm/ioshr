00bd76811bb87d2445c3db95c85e3438c7836e38
Manas Ranjan Mohanty <coeaimrm@cet.edu.in>
2020-07-10 08:31:01 +0530

    test change

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firestore_crud/main.dart';
+import 'package:hrs/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
